0e8ff158fc83d2166943ca8730bc955ae9a8ecd8
250541063-blip <250541063@firat.edu.tr>
2025-10-27 10:18:15 +0300

    Update GeometrikHesap.java

--- GeometrikHesap.java
+++ GeometrikHesap.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
+
+public class DeltoidAlanHesaplama {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("=== Deltoid (Eşkenar Dörtgen) Alan Hesaplama ===");
+        
+        // Kullanıcıdan köşegen uzunluklarını alıyoruz
+        System.out.print("Birinci köşegen uzunluğunu girin (d1): ");
+        double d1 = input.nextDouble();
+
+        System.out.print("İkinci köşegen uzunluğunu girin (d2): ");
+        double d2 = input.nextDouble();
+
+        // Alan formülü: (d1 * d2) / 2
+        double alan = (d1 * d2) / 2;
+
+        System.out.println("Deltoidin Alanı: " + alan);
+
+        input.close();
+    }
+}
 
